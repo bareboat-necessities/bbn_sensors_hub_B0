de2306503ba870e822fedec556172ffc1617653c
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-12-11 09:29:58 -0500

    Update bbn_esp32_sensors_hub.ino

--- bbn_esp32_sensors_hub.ino
+++ bbn_esp32_sensors_hub.ino
@@ -1,5 +1,14 @@
+#include <M5AtomS3.h>
+
+#include "NmeaXDR.h"
+#include "Nmea0183Msg.h"
+
 void setup() {
+  auto cfg = M5.config();
+  AtomS3.begin(cfg);
+  Serial.begin(115200);
 }
 
 void loop() {
+  delay(3);
 }
