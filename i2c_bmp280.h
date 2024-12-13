1e1a05a3610a3084281e923abba40d3349ad0760
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-12-12 23:15:50 -0500

    upd

--- i2c_bmp280.h
+++ i2c_bmp280.h
@@ -16,4 +16,26 @@ void i2c_bmp280_report() {
   }
 }
 
+void i2c_bmp280_try_init() {
+  for (int i = 0; i < 3; i++) {
+    i2c_bmp280_found = i2c_bmp280_sensor.begin(&Wire, BMP280_I2C_ADDR, G2, G1, 400000U);
+    if (i2c_bmp280_found) {
+      break;
+    }
+    delay(20);
+  }
+  if (i2c_bmp280_found) {
+    gen_nmea0183_msg("$BBTXT,01,01,01,ENVIRONMENT found bmp280 sensor at address=0x%s", String(BMP280_I2C_ADDR, HEX).c_str());
+    /* Default settings from datasheet. */
+    i2c_bmp280_sensor.setSampling(BMP280::MODE_NORMAL,     /* Operating Mode. */
+                                  BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                                  BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                                  BMP280::FILTER_X16,      /* Filtering. */
+                                  BMP280::STANDBY_MS_500); /* Standby time. */
+    app.onRepeat(5000, []() {
+      i2c_bmp280_report();
+    });
+  }
+}
+
 #endif
