c6424dfec98a82b800dfaa1791bba9c03c0b485d
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-12-12 23:17:56 -0500

    upd

--- i2c_sensors.h
+++ i2c_sensors.h
@@ -3,11 +3,15 @@
 
 #include "i2c_sht30.h"
 #include "i2c_qmp6988.h"
+#include "i2c_bmp280.h"
+#include "i2c_dht12.h"
 #include "i2c_ads1115.h"
 
 void i2c_sensors_scan() {
   i2c_ads1115_try_init();
   i2c_sht30_try_init();
+  i2c_dht12_try_init();
+  i2c_bmp280_try_init();
   i2c_qmp6988_try_init();
 }
 
