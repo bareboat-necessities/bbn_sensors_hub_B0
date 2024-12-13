319ff083a2d0610229c46726be453c6a46c746ea
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-12-12 19:46:04 -0500

    wip

--- i2c_sensors.h
+++ i2c_sensors.h
@@ -1,9 +1,9 @@
 #ifndef i2c_sensors_h
 #define i2c_sensors_h
 
-#include "i2c_ads1115.h"
 #include "i2c_sht30.h"
 #include "i2c_qmp6988.h"
+#include "i2c_ads1115.h"
 
 void i2c_sensors_scan() {
   i2c_ads1115_try_init();
