72bd904cb824ea004c723d19a453bbbc37317592
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-12-12 23:02:21 -0500

    fix

--- i2c_bmp280.h
+++ i2c_bmp280.h
@@ -9,4 +9,11 @@
 BMP280 i2c_bmp280_sensor;
 bool i2c_bmp280_found = false;
 
+void i2c_bmp280_report() {
+  if (i2c_bmp280_sensor.update()) {
+    gen_nmea0183_xdr("$BBXDR,C,%.2f,C,TEMP_BMP280", i2c_bmp280_sensor.cTemp);        // C
+    gen_nmea0183_xdr("$BBXDR,P,%.2f,P,PRES_BMP280", i2c_bmp280_sensor.pressure);     // Pa
+  }
+}
+
 #endif
--- i2c_dht12.h
+++ i2c_dht12.h
@@ -9,4 +9,11 @@
 DHT12 i2c_dht12_sensor;
 bool i2c_dht12_found = false;
 
+void i2c_dht12_report() {
+  if (i2c_dht12_sensor.update()) {
+    gen_nmea0183_xdr("$BBXDR,H,%.2f,P,HUMI_DHT12", i2c_dht12_sensor.humidity);   // %
+    gen_nmea0183_xdr("$BBXDR,C,%.2f,C,TEMP_DHT12", i2c_dht12_sensor.cTemp);      // C
+  }
+}
+
 #endif
