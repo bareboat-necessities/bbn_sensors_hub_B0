73c39a6bfc153320aefd1b922516788c2818749a
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-12-12 22:59:29 -0500

    fix

---
+++ i2c_bmp280.h
@@ -0,0 +1,12 @@
+#ifndef i2c_bmp280_h
+#define i2c_bmp280_h
+
+#include <M5UnitENV.h>
+
+#include "NmeaXDR.h"
+#include "Nmea0183Msg.h"
+
+BMP280 i2c_bmp280_sensor;
+bool i2c_bmp280_found = false;
+
+#endif
---
+++ i2c_dht12.h
@@ -0,0 +1,12 @@
+#ifndef i2c_dht12_h
+#define i2c_dht12_h
+
+#include <M5UnitENV.h>
+
+#include "NmeaXDR.h"
+#include "Nmea0183Msg.h"
+
+DHT12 i2c_dht12_sensor;
+bool i2c_dht12_found = false;
+
+#endif
