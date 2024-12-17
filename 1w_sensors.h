14c7fa12effc12b9159bb8af377e32653258069f
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-12-17 08:54:51 -0500

    upd

--- 1w_sensors.h
+++ 1w_sensors.h
@@ -3,10 +3,10 @@
 
 #include <OneWire.h>
 
-#include "1w_ds18b20.h"
-
 #define ONE_WIRE_BUS G8          // Pin to which 1-wire bus is connected to esp32
 OneWire oneWire(ONE_WIRE_BUS);
+
+#include "1w_ds18b20.h"
 
 void one_wire_sensors_scan() {
   one_wire_dallas_try_init();
