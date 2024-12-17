066c473f881dd7089a7752307dd88ccc64805984
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-12-17 11:56:36 -0500

    upd

--- i2c_sensors.h
+++ i2c_sensors.h
@@ -10,20 +10,22 @@
 #include "i2c_bh1750fvi_tr.h"
 #include "i2c_ain_4_20ma.h"
 #include "i2c_sgp30.h"
+#include "i2c_vl53l0x.h"
 
 void i2c_sensors_scan(bool i2c_alt_enable_scan) {
   i2c_ads1115_try_init(&Wire, G2, G1, 100000UL);
   if (i2c_alt_enable_scan) {
     i2c_ads1115_try_init(&Wire1, G38, G39, 100000UL);
   }
-  i2c_sht30_try_init();
-  i2c_sgp30_try_init();
-  i2c_dht12_try_init();
   i2c_bmp280_try_init();
   i2c_qmp6988_try_init();
   i2c_bme680_try_init();
-  i2c_bh1750fvi_tr_try_init();
   i2c_ain_4_20ma_try_init();
+  i2c_dht12_try_init();
+  i2c_sht30_try_init();
+  i2c_sgp30_try_init();
+  i2c_bh1750fvi_tr_try_init();
+  i2c_vl53l0x_try_init();
 }
 
 void i2c_sensors_update() {
