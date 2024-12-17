9ab53087f86c5e648dd939a8e644cbf1fd14967b
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-12-17 08:48:35 -0500

    upd

---
+++ 1w_ds18b20.h
@@ -0,0 +1,62 @@
+#ifndef w1_ds18b20_h
+#define w1_ds18b20_h
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#include "NmeaXDR.h"
+#include "Nmea0183Msg.h"
+
+DallasTemperature sensors(&oneWire);
+
+void byte_to_hex_ascii(unsigned char byte, char hex_str[3]) {
+  sprintf(hex_str, "%02X", byte);
+}
+
+void addressToStr(DeviceAddress deviceAddress, char str[17]) {
+  for (uint8_t i = 0; i < 8; i++) {
+    char a[3];
+    byte_to_hex_ascii(deviceAddress[i], a);
+    str[2 * i] = a[0];
+    str[2 * i + 1] = a[1];
+  }
+  str[16] = (char)0;
+}
+
+void one_wire_dallas_sensors_report() {
+  int count = sensors.getDeviceCount();
+  if (count > 0) {
+    sensors.requestTemperatures();
+    for (int i = 0; i < count; i++) {
+      DeviceAddress deviceAddress;
+      if (sensors.getAddress(deviceAddress, i)) {
+        char str[17];
+        addressToStr(deviceAddress, str);
+        gen_nmea0183_msg("$BBXDR,C,%s", (String(sensors.getTempCByIndex(i)) + ",C,TEMP_" + String(str)).c_str());  // C
+      }
+    }
+  }
+}
+
+void one_wire_dallas_try_init() {
+  sensors.begin();
+  int count = sensors.getDeviceCount();
+  if (count > 0) {
+    gen_nmea0183_msg("$BBTXT,01,01,01,TEMPERATURE sensors found=%s", String(count).c_str());
+    for (int i = 0; i < count; i++) {
+      DeviceAddress deviceAddress;
+      if (!sensors.getAddress(deviceAddress, i)) {
+        gen_nmea0183_msg("$BBTXT,01,01,02,TEMPERATURE unable to find sensor address for device=%s", String(i).c_str());
+      } else {
+        char str[17];
+        addressToStr(deviceAddress, str);
+        gen_nmea0183_msg("$BBTXT,01,01,01,TEMPERATURE found sensor address=%s", str);
+        app.onRepeat(5000, []() {
+          one_wire_dallas_sensors_report();
+        });
+      }
+    }
+  }
+}
+
+#endif
--- 1w_sensors.h
+++ 1w_sensors.h
@@ -2,63 +2,14 @@
 #define w1_sensors_h
 
 #include <OneWire.h>
-#include <DallasTemperature.h>
 
-#include "Nmea0183Msg.h"
+#include "1w_ds18b20.h"
 
 #define ONE_WIRE_BUS G8          // Pin to which 1-wire bus is connected to esp32
 OneWire oneWire(ONE_WIRE_BUS);
 
-DallasTemperature sensors(&oneWire);
-
-void byte_to_hex_ascii(unsigned char byte, char hex_str[3]) {
-  sprintf(hex_str, "%02X", byte);
-}
-
-void addressToStr(DeviceAddress deviceAddress, char str[17]) {
-  for (uint8_t i = 0; i < 8; i++) {
-    char a[3];
-    byte_to_hex_ascii(deviceAddress[i], a);
-    str[2 * i] = a[0];
-    str[2 * i + 1] = a[1];
-  }
-  str[16] = (char)0;
-}
-
-void one_wire_sensors_report() {
-  int count = sensors.getDeviceCount();
-  if (count > 0) {
-    sensors.requestTemperatures();
-    for (int i = 0; i < count; i++) {
-      DeviceAddress deviceAddress;
-      if (sensors.getAddress(deviceAddress, i)) {
-        char str[17];
-        addressToStr(deviceAddress, str);
-        gen_nmea0183_msg("$BBXDR,C,%s", (String(sensors.getTempCByIndex(i)) + ",C,TEMP_" + String(str)).c_str());  // C
-      }
-    }
-  }
-}
-
 void one_wire_sensors_scan() {
-  sensors.begin();
-  int count = sensors.getDeviceCount();
-  if (count > 0) {
-    gen_nmea0183_msg("$BBTXT,01,01,01,TEMPERATURE sensors found=%s", String(count).c_str());
-    for (int i = 0; i < count; i++) {
-      DeviceAddress deviceAddress;
-      if (!sensors.getAddress(deviceAddress, i)) {
-        gen_nmea0183_msg("$BBTXT,01,01,02,TEMPERATURE unable to find sensor address for device=%s", String(i).c_str());
-      } else {
-        char str[17];
-        addressToStr(deviceAddress, str);
-        gen_nmea0183_msg("$BBTXT,01,01,01,TEMPERATURE found sensor address=%s", str);
-        app.onRepeat(5000, []() {
-          one_wire_sensors_report();
-        });
-      }
-    }
-  }
+  one_wire_dallas_try_init();
 }
 
 #endif
