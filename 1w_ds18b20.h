a29a9275db02172c5f62e75e35bfb338743d986a
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-12-17 18:08:50 -0500

    upd

--- 1w_ds18b20.h
+++ 1w_ds18b20.h
@@ -42,15 +42,15 @@ void one_wire_dallas_try_init() {
   sensors.begin();
   int count = sensors.getDeviceCount();
   if (count > 0) {
-    gen_nmea0183_msg("$BBTXT,01,01,01,TEMPERATURE sensors found=%s", String(count).c_str());
+    gen_nmea0183_msg("$BBTXT,01,01,01,TEMPERATURE 1wire sensors found=%s", String(count).c_str());
     for (int i = 0; i < count; i++) {
       DeviceAddress deviceAddress;
       if (!sensors.getAddress(deviceAddress, i)) {
-        gen_nmea0183_msg("$BBTXT,01,01,02,TEMPERATURE unable to find sensor address for device=%s", String(i).c_str());
+        gen_nmea0183_msg("$BBTXT,01,01,02,TEMPERATURE unable to find 1wire sensor address for device=%s", String(i).c_str());
       } else {
         char str[17];
         addressToStr(deviceAddress, str);
-        gen_nmea0183_msg("$BBTXT,01,01,01,TEMPERATURE found sensor address=%s", str);
+        gen_nmea0183_msg("$BBTXT,01,01,01,TEMPERATURE found 1wire sensor address=%s", str);
         app.onRepeat(5000, []() {
           one_wire_dallas_sensors_report();
         });
