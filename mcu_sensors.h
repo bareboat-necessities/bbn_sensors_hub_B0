c27807e55ee9b01ad7592daf38309f28654789d9
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-03-03 10:54:24 -0500

    Update mcu_sensors.h

--- mcu_sensors.h
+++ mcu_sensors.h
@@ -1,7 +1,6 @@
 #ifndef mcu_sensors_h
 #define mcu_sensors_h
 
-#include "i2c_sht30.h"
 #include "i2c_qmp6988.h"
 #include "i2c_bmp280.h"
 #include "i2c_bme680.h"
@@ -12,9 +11,7 @@
 #include "i2c_sgp30.h"
 #include "i2c_vl53l0x.h"
 #include "i2c_ina219.h"
-#include "gpio_pir_as312.h"
 #include "gpio_tds_cqrsentds01.h"
-#include "gpio_limit_switch.h"
 #include "gpio_240_30_ohm_sensor.h"
 
 void mcu_sensors_scan() {
@@ -25,13 +22,10 @@ void mcu_sensors_scan() {
   i2c_qmp6988_try_init();
   i2c_bme680_try_init();
   i2c_dht12_try_init();
-  i2c_sht30_try_init();
   i2c_sgp30_try_init();
   i2c_bh1750fvi_tr_try_init();
   i2c_vl53l0x_try_init();
-  gpio_pir_as312_try_init();
   gpio_tds_cqrsentds01_try_init();
-  gpio_limit_switch_try_init();
   gpio_240_30_ohm_try_init();
 }
 
